f19999134f9394acc9761f42722c3de31f3b5b27
enekassooo <81808947+enekassooo@users.noreply.github.com>
2024-05-17 20:00:02 +0200

    Hemos añadido el diseño a la aplicacion

---
+++ app/src/main/java/com/example/funphoto/ImageHelper.java
@@ -0,0 +1,29 @@
+package com.example.funphoto;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.PorterDuff;
+
+public class ImageHelper {
+    // Método para obtener una imagen circular
+    public static Bitmap getCircularBitmap(Bitmap bitmap) {
+        int width = bitmap.getWidth();
+        int height = bitmap.getHeight();
+        int radius = Math.min(width, height) / 2;
+        Bitmap output = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, width, height);
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(Color.WHITE);
+        canvas.drawCircle(width / 2f, height / 2f, radius, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+        return output;
+    }
+}
